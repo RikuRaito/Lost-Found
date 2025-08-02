b6cb0e74ec7b5b4efadb1364289307ba155ada69
taitai03 <ttatatai.gagaga@gmail.com>
2025-08-02 17:11:46 +0900

    make item-details

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import Notification from './pages/Notification'
 import AdminLogin from './pages/AdminLogin'
 import AnimatedLinesCircle from './components/Animation'
 import SearchResult from './pages/SearchResult'
+import ItemDetails from './pages/ItemDetails'
 
 function App() {
   const location = useLocation();
@@ -31,6 +32,7 @@ function App() {
           <Route path='/Notification' element={<Notification />} />
           <Route path='/New_items' element={<NewItems />} />
           <Route path='/Search_results' element={<SearchResult/>} />
+          <Route path='/item/:itemId' element={<ItemDetails />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/Signup" element={<Signup />} />
           <Route path="*" element={<Top /> } />
